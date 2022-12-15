c9b06666502812548758666d6d9099869faa660c
Darya <daryakuzmich0@gmail.com>
2022-12-16 02:24:45 +0300

    Задание 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+string numberStr = number.ToString();
+int sum = 0;
+for (int i = 0; i < numberStr.Length; i++)
+{
+    sum = sum + (numberStr[i]-'0');
+}
+Console.WriteLine(sum);
